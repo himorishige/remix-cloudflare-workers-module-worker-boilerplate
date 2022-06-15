59609ff5f0a78105e338a0e8e107641f408f23b1
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-06-15 23:26:07 +0900

    update: root

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,9 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from '@remix-run/react';
+import type { PropsWithChildren } from 'react';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -14,7 +16,7 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
-export default function App() {
+const Document = ({ children }: PropsWithChildren<{}>) => {
   return (
     <html lang="en">
       <head>
@@ -22,11 +24,44 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
+};
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
 }
+
+export const CatchBoundary = () => {
+  let { status, statusText } = useCatch();
+
+  return (
+    <Document>
+      <main>
+        <h1>{status}</h1>
+        {statusText && <p>{statusText}</p>}
+      </main>
+    </Document>
+  );
+};
+
+export const ErrorBoundary = ({ error }: { error: Error }) => {
+  console.log(error);
+
+  return (
+    <Document>
+      <main>
+        <h1>Oops, looks like something went wrong 😭</h1>
+      </main>
+    </Document>
+  );
+};
